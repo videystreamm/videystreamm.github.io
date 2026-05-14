a82144725f404e94ec4147db4e6d2432fceec85f
videystreamm <nbi34148@gmail.com>
2026-05-15 01:52:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server jalan 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("running");
+});
